8cd73682526fd74f704987564832e1c95fd42e8d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2017-10-11 10:23:14 +0900

    Update video_player_test

--- video_player_test/main_3d.gd
+++ video_player_test/main_3d.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Spatial
 
 func play():
 	get_node("video_player").play()
@@ -12,11 +12,18 @@ func pause():
 func unpause():
 	get_node("video_player").set_paused(false)
 
+func load_stream(stream):
+	var player = get_node("video_player")
+	player.set_stream(stream)
+	get_node("cube").get_surface_material(0).set_texture(
+			SpatialMaterial.TEXTURE_ALBEDO,
+			player.get_video_texture())
+
 func load_webm():
-	get_node("video_player").set_stream(preload("res://llamigos-small.webm"))
+	load_stream(preload("res://llamigos-small.webm"))
 
 func load_ogv():
-	get_node("video_player").set_stream(preload("res://llamigos-small.ogv"))
+	load_stream(preload("res://llamigos-small.ogv"))
 
 func go_to_2d():
 	stop()
